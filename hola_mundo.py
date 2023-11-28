4e1b8233e829e8ae01f1eb36da9bc3de73a0211c
Angelfega <ferrinangel01@gmail.com>
2023-11-28 10:49:33 +0100

    Cambiamos el print

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,3 +1,3 @@
 print('Hola mundo! He modificado este documento desde github')
 for i in range(0, 50):
-    print("Corregire los examenes a tiempo")
+    print("En serio, corregire los examenes algun dia")
